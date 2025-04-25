c28c3e997184603adbd5aa0b45d326a140d7bd8d
Tyler Davenport <tydavenport0415@gmail.com>
2025-04-24 19:37:33 -0500

    delete and update

--- request_handler.py
+++ request_handler.py
@@ -139,6 +139,8 @@ class HandleRequests(BaseHTTPRequestHandler):
         post_body = json.loads(post_body)
 
         (resource, id) = self.parse_url(self.path)
+        
+        success = False
 
         if resource == "animals":
             update_animal(id, post_body)
@@ -148,6 +150,11 @@ class HandleRequests(BaseHTTPRequestHandler):
             update_employee(id, post_body)
         elif resource == "customers":
             update_customer(id, post_body)
+            
+            if success:
+                self._set_headers(204)
+            else:
+                self._set_headers(404)
 
         self.wfile.write("".encode())
 
